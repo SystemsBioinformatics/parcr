37e5eba50a352bdf939519fd0a19856936c170ed
douwe <d.molenaar@vu.nl>
2023-12-27 17:12:54 +0100

    delete unused function

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,3 @@
-# !is.null(l) && length(l)==0 # only list()
-
 #' Testing whether a list is empty
 #'
 #' @param l A list
@@ -8,26 +6,4 @@
 #' @keywords internal
 is.empty <- function(l) {
   length(l) == 0
-}
-
-#' Aggregate two parselists
-#'
-#' The function `agg` aggregates two parseLR lists.
-#'
-#' @details
-#' It corresponds to the Miranda `++` notation. The empty list `list()` is the
-# TODO: Does it actually correspond to ++, does Miranda have the concept of list of lists?
-#' unit element for this function, meaning that if either of the two arguments
-#' is an empty list then it will not be present in the result, and if both are
-#' empty then the result will be the empty list.
-#'
-#' @param l1,l2 Two parseLR lists
-#'
-#' @returns A list
-#' @keywords internal
-#'
-agg <- function(l1, l2) {
-  result <- list(l1, l2)
-  result <- result[sapply(result, function(x) {!is.empty(x)})]
-  return(result)
 }
--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -2,13 +2,3 @@ test_that("'is.empty' works", {
   expect_true(is.empty(list()))
   expect_false(is.empty(list(x='ab')))
 })
-
-test_that("List aggregation works with non-empty lists", {
-  expect_equal(agg(list('abc','def'), list('ghi','jkl')), list(list('abc','def'), list('ghi','jkl')))
-})
-
-test_that("'list()' is the unit element of list aggregation", {
-  expect_equal(agg(list('abc','def'), list()), list(list('abc','def')))
-  expect_equal(agg(list(), list('abc','def')), list(list('abc','def')))
-  expect_equal(agg(list(), list()), list())
-})
