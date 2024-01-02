340efcd27a0f8c217f2b3ce1687013c3749fdf36
douwe <d.molenaar@vu.nl>
2024-01-02 19:06:00 +0100

    extra test for by_split

--- tests/testthat/test-string_parsers.R
+++ tests/testthat/test-string_parsers.R
@@ -1,9 +1,12 @@
+starts_with_a <- function(x) grepl("^a",x[1])
+
 test_that("'by_split' works in standard cases", {
   expect_equal(by_split((literal("a") %then% literal("b")),"\\t") ("a\tb"), list(L=c(list("a"),list("b")), R=character(0)))
   expect_equal(by_split((literal("a") %then% literal("b")),"\\t") ("a\tb\tc"), list())
   expect_equal(by_split((literal("a") %then% literal("b")), "\\t", finish=FALSE) ("a\tb\tc"), list(L=c(list("a"),list("b")), R=character(0)))
   expect_equal(by_split((literal("a") %then% literal("b")), "") ("ab"), list(L=c(list("a"),list("b")), R=character(0)))
   expect_equal(by_split(exactly(3,literal("a")), "\\t") ("a\ta\ta"), list(L=c(list("a"),list("a"), list("a")), R=character(0)))
+  expect_equal(by_split(exactly(3,satisfy(starts_with_a)), ",", fixed=TRUE)('atggc,acggg,acttg'), list(L=c(list("atggc"), list("acggg"), list("acttg")), R=character(0)))
 })
 
 test_that("'by_symbol' works in standard cases", {
