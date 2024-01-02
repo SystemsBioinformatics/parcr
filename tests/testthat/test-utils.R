4aa6c8ad804335a00b20db25c09d7a44e58b41cb
douwe <d.molenaar@vu.nl>
2024-01-02 22:00:34 +0100

    deleted obsolete fst and snd functions

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -9,13 +9,6 @@ test_that("'is.empty' and 'failed' work", {
   expect_false(failed('ab'))
 })
 
-test_that("'fst' and 'snd' work", {
-  expect_equal(fst(c('a','b')), 'a')
-  expect_equal(snd(c('a','b')), 'b')
-  expect_equal(fst(c('a')), 'a')
-  expect_equal(snd(c('a')), NULL)
-})
-
 test_that("'ensure.list' works", {
   expect_equal(ensure.list(data.frame(a='a')), list(data.frame(a='a')))
 })
