3ca9a107063668708cf304be67b21665a92db21a
douwe <d.molenaar@vu.nl>
2024-01-05 09:49:50 +0100

    document fastafile data

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Example nucleotide fasta file
+#'
+#' An example fasta-formatted file with multiple nucleotide sequences. It is
+#' used in the vignette to demonstrate parsing with the tools from the package.
+#'
+#' @format ## `fastafile`
+#' A character vector
+#' @source Modified from <https://bioinformatics.org/annhyb/examples/seq_fasta.html>
+"fastafile"
