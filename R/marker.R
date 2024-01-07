09b54c4283d301036f47a48776fe529fc02f204d
douwe <d.molenaar@vu.nl>
2024-01-07 08:32:30 +0100

    document LNR

--- R/marker.R
+++ R/marker.R
@@ -8,16 +8,24 @@ new_marker <- function(n) {
 
 marker_val <- function(marker) {attr(marker,"n")}
 
+#' The `the` environment is used for keeping track of the state variable `LNR`.
+#' @keywords internal
 the <- list2env(list(LNR = 1L), parent = emptyenv())
 
+#' @describeIn the Return the value of `LNR`.
 LNR <- function() {the$LNR}
 
+#' @param n an `integer`, will be coerced to `integer`
+#' @describeIn the Set the value of `LNR` to `n`.
 set_LNR <- function(n) {the$LNR <- as.integer(n)}
 
+#' @describeIn the Increase the value of `LNR` by 1.
 inc_LNR <- function() {the$LNR <- the$LNR + 1L}
 
+#' @describeIn the Decrease the value of `LNR` by 1.
 dec_LNR <- function() {the$LNR <- the$LNR - 1L}
 
+#' @describeIn the Reset the value of `LNR` to 1.
 reset_LNR <- function() {set_LNR(1L)}
 
 #' Prints an object of class `marker`
