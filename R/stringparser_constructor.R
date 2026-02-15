ae6abea1b89801293f14ce32299d1ba9262ebe7e
Douwe Molenaar <d.molenaar@vu.nl>
2026-02-15 13:06:43 +0100

    stringparser is now independent of stringr package

--- R/stringparser_constructor.R
+++ R/stringparser_constructor.R
@@ -1,3 +1,10 @@
+#' Helper function emulating `stringr::str_match()`
+#'
+#' @noRd
+str_match <- function(line, match_pattern, ...) {
+  regmatches(line, regexec(match_pattern, line, ...))[[1]]
+}
+
 #' String parser constructor
 #'
 #' @description
@@ -7,7 +14,7 @@
 # @usage parse_header = stringparser(match_pattern = "^>(\\w+)", reshape = function(x) {x})
 #'
 #' @details
-#' This function uses [stringr::str_match()] to produce a string parser. Parsers
+#' This function uses [regexec()] to produce a string parser. Parsers
 #' created with this constructor return the failure signal `list()` when a
 #' string does not match the `match_pattern`. If the pattern contains captured
 #' groups then these groups will be returned as a character vector upon matching.
@@ -23,38 +30,40 @@
 #' @param match_pattern A regular expression that matches the string.
 #'
 #' @param reshape A function that takes the character vector of captured strings
-#' and modifies it to a desired output. By default this is the identity function.
+#' and modifies it to a desired output. By default this is the [identity()]
+#' function.
 #'
-#' @seealso [match_s()], [stringr::str_match()]
+#' @param ... additional parameters passed on to [regexec()]
+#'
+#' @seealso [match_s()], [regexec()]
 #'
 #' @export
 #'
 #' @examples
 #' # single capture group
 #' parse_header <- stringparser("^>(\\w+)")
-#' parse_header(">correct_header")     # returns "correct_header"
-#' parse_header("> incorrect_header")  # returns list()
+#' parse_header(">correct_header") # returns "correct_header"
+#' parse_header("> incorrect_header") # returns list()
 #'
 #' # multiple capture groups
 #' parse_keyvalue <- stringparser("(\\w+):\\s?(\\w+)")
-#' parse_keyvalue("key1: value1")      # returns c("key1", "value1")
+#' parse_keyvalue("key1: value1") # returns c("key1", "value1")
 #'
 #' # modify output
-#' parse_keyvalue_df <- stringparser("(\\w+):\\s?(\\w+)",
-#'                                    function(x) data.frame(key = x[1], value = x[2])
-#'                                  )
-#' parse_keyvalue_df("key1: value1")      # returns a data frame
+#' parse_keyvalue_df <- stringparser(
+#'   "(\\w+):\\s?(\\w+)",
+#'   function(x) data.frame(key = x[1], value = x[2])
+#' )
+#' parse_keyvalue_df("key1: value1") # returns a data frame
 #'
-stringparser <- function(match_pattern, reshape = function(x) {x}) {
+stringparser <- function(match_pattern, reshape = identity, ...) {
   function(line) {
-    m <- stringr::str_match(line, match_pattern)
-    if (is.na(m[1])) {
+    m <- str_match(line, match_pattern, ...)
+    if (length(m) == 0) {
       return(list()) # signal failure
     } else {
-      m <- m[-1]
-      if (length(m) > 0) {
-        return(reshape(m))
-      }
+      m <- m[-1] # Remove full match, keep capture groups
+      return(reshape(m))
     }
   }
 }
